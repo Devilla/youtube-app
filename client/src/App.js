48a4b8a526c9ce701b86dd49e2bff90efb07a19e
devilla <dev.koold@gmail.com>
2018-10-30 21:19:59 +0530

    react client upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import {  ApolloProvider } from 'react-apollo';
-import BookList from './components/BookList.js';
+// import BookList from './components/BookList.js';
 
 const client = new  ApolloClient({
   uri:'http://localhost:4000/graphql'
@@ -12,9 +12,9 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
       <div id="main">
-      <h1>MY App</h1>
+      <h1>Upload Video</h1>
+      <input type="file"/>
       </div>
-      <BookList/>
       </ApolloProvider>
     );
   }
