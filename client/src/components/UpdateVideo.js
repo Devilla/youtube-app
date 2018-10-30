a15787049fd73169a572da88c15be1df56f87599
devilla <dev.koold@gmail.com>
2018-10-31 02:39:53 +0530

    state added

--- client/src/components/UpdateVideo.js
+++ client/src/components/UpdateVideo.js
@@ -11,15 +11,25 @@ const getBooksQuery = gql`
 `
 
 class UpdateVideo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videoID : "k12SKt794YU",
+      videoTitle : "",
+      videoDescription : "",
+      videoPrivacy : ""
+    };
+  }
   render(){
+    const { videoID , videoTitle, videoDescription,videoPrivacy } = this.state;
     console.log(this.props);
     return (
       <div>
       <h3>Update Video details</h3>
-      <input type="text" placeholder="Enter unique video ID"/>
-      <input type="text" placeholder="Enter title"/>
-      <input type="text" placeholder="Enter description"/>
-      <input type="text" placeholder="Enter video privacy"/>
+      <input type="text" placeholder="Enter unique video ID" value={videoID}/>
+      <input type="text" placeholder="Enter title" value={videoTitle}/>
+      <input type="text" placeholder="Enter description" value={videoDescription}/>
+      <input type="text" placeholder="Enter video privacy public/private" value={videoPrivacy}/>
       <input type="submit"/>
       </div>
     );
